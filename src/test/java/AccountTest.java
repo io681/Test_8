124a97aa3f283bcc60426eacb084b7ce685ffe83
io681 <io-68@yandex.ru>
2023-04-15 21:20:22 +0300

    Updates for Allure

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,24 +1,31 @@
+import io.qameta.allure.junit4.DisplayName;
 import org.example.Account;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+@DisplayName("Тесты валидации аккаунта")
 public class AccountTest {
     private String name;
     private Account account;
-//    @Before
-//    public void createAccount (){
-//        account = new Account(name);
-//    }
 
     @Test
+    @DisplayName("Позитивный тест")
+    public void accountSuccessTrueTest(){
+        account = new Account("nas Tya");
+        boolean result = account.checkNameToEmboss();
+        assertEquals("Ошибка сравнения", true, result);
+    }
+    @Test
+    @DisplayName("Проверка максимального значения символов")
     public void accountNameGreaterThan19ShowsFalseTest(){
         account = new Account("name Name1234567890123");
         boolean result = account.checkNameToEmboss();
         assertEquals("Ошибка сравнения", false, result);
     }
     @Test
+    @DisplayName("Проверка минимального значения символов")
     public void accountNameLessThan3ShowsFalseTest(){
         account = new Account("na");
         boolean result = account.checkNameToEmboss();
